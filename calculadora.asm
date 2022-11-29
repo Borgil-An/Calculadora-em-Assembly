4e5da031e7b0980f40cc84ea4dade8e380f6a5df
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2022-11-28 22:33:51 -0300

    Update calculadora.asm
    Tem que adicionar o 1 manualmente ao 253

--- calculadora.asm
+++ calculadora.asm
@@ -1,12 +1,13 @@
-; Calculadora em Assembly
-; Escolha 1 para realizar uma soma.
-; Escolha 2 para realizar uma subtracao.
-; Escolha 3 para realizar uma multiplicacao.
-; Escolha 4 para realizar uma divisao.
-; Escolha 5 para realizar um somatorio.
-; Para sair da calculadora escolha 6.
-inicio: IN 0
-STA 255       ;255 serve para controlar a opção
+inicio: LDA 250 ; Calculadora em Assembly
+NOT ; Escolha 1 para realizar uma soma.
+ADD 253 ; Escolha 2 para realizar uma subtracao.
+ADD 250 ; Escolha 3 para realizar uma multiplicacao.
+STA 250 ; Escolha 4 para realizar uma divisao. 
+IN 1 ; Escolha 5 para realizar um somatorio. 
+ADD 251 ; Para sair da calculadora escolha 6.
+JZ inicio
+IN 0 
+STA 255      ;255 serve para controlar a opção
 NOT          
 ADD 253
 ADD 253       ;253 numero 1 de apoio
@@ -22,7 +23,6 @@ JZ som
 ADD 253
 JZ fim
 fim: HLT
-
 ;Operacao de Soma
 soma: IN 1
 ADD 251 ; 251 numero 0 de apoio
@@ -37,10 +37,9 @@ STA 252
 LDA 254
 ADD 252
 STA 250 ; 250 guarda o resultado da operacao
+OUT 0
 ;Fim da operacao de Soma
 JMP inicio
-
-
 ;Operacao de Sub
 sub: IN 1
 ADD 251
@@ -56,10 +55,9 @@ NOT
 ADD 253
 ADD 254
 STA 250
+OUT 0
 ;Fim da operacao de Sub
 JMP inicio
-
-
 ;Comeco da operacao de Multiplicacao
 mult: IN 1
 ADD 251
@@ -71,21 +69,27 @@ ADD 251
 JZ nume3
 IN 0 
 STA 252
-
-multloop: LDA 240
+multloop: LDA 250
 ADD 254
-STA 240
+STA 250
+LDA 249
+ADD 253
+STA 249
 LDA 252
-NOT 
+NOT
 ADD 253
-ADD 253
-STA 253
+ADD 249
 JNZ multloop
-; Final da operacao de Multiplicacao
-JMP inicio
-
-
-; Comeco da operacao de Divisao
+LDA 250
+OUT 0
+LDA 249 ;Para zerar o meu "resultado de apoio"
+NOT
+ADD 253
+ADD 249
+STA 249
+JZ inicio
+;Final da operacao de multiplicacao
+;Comeco da operacao de Divisao
 div: IN 1
 ADD 251
 JZ div
@@ -96,35 +100,37 @@ ADD 251
 JZ nume4
 IN 0 
 STA 252
-
-divloop: LDA 254
+LDA 254
 NOT
 ADD 253
+STA 254
+divloop: LDA 254
 ADD 252
 STA 254
 LDA 250
 ADD 253
 STA 250
 LDA 254
+JN divloop
+LDA 250
+OUT 0 
+LDA 254
 JZ inicio
-JMP divloop
-; Final da operacao de Divisao
-
-; Comeco da operacao de Somatoria
+;Final da operacao de Divisao
+;Comeco da operacao de Somatoria
 som: IN 1
 ADD 251
 JZ som
 IN 0 
 STA 254
-
 somloop: LDA 250
 ADD 253
 STA 250
+OUT 0
 LDA 254
 NOT
 ADD 253
 ADD 250
 JNZ somloop
-; Final do somatorio
 JMP inicio
 
